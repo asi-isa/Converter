b4edf9fa0dacfc6fcd2e7c82e772fd1243bc02a1
asi-isa <ali.inceoglu@protonmail.com>
2025-09-07 23:25:27 +0200

    Converter

--- Converter/ContentView.swift
+++ Converter/ContentView.swift
@@ -8,14 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let units: [UnitDuration] = [
+        .hours,
+        .minutes,
+        .seconds,
+        .milliseconds
+    ]
+    @FocusState private var isAmountFocused: Bool
+    @State private var sourceUnit: UnitDuration = .minutes
+    @State private var targetUnit: UnitDuration = .seconds
+    @State private var amount = 1.0
+    private var targetAmount: Double {
+        let sourceValue = Measurement(value: amount, unit: sourceUnit)
+        return sourceValue.converted(to: targetUnit).value
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section("Source Unit") {
+                    Picker("Source Unit", selection: $sourceUnit) {
+                        ForEach(units, id: \.self) { unit in
+                            Text(unit.displayName).tag(unit)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section("Target Unit") {
+                    Picker("Target Unit", selection: $targetUnit) {
+                        ForEach(units, id: \.self) { unit in
+                            Text(unit.displayName).tag(unit)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section("Amount") {
+                    TextField("Amount", value: $amount, format: .number)
+                        .keyboardType(.numberPad)
+                        .focused($isAmountFocused)
+                }
+                
+                Section("Converted Amount") {
+                    Text(targetAmount, format: .number)
+                }
+                
+            }
+            .navigationTitle("Converter")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                if isAmountFocused {
+                    Button("Done") {
+                        isAmountFocused = false
+                    }
+                }
+            }
         }
-        .padding()
+    }
+}
+
+extension UnitDuration {
+    var displayName: String {
+        switch self {
+        case .hours: return "Hours"
+        case .minutes: return "Minutes"
+        case .seconds: return "Seconds"
+        case .milliseconds: return "Milliseconds"
+        default: return self.symbol
+        }
     }
 }
 
