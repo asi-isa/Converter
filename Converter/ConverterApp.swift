2a15e4818edee4e809840539eab3377336a55374
asi-isa <ali.inceoglu@protonmail.com>
2025-09-07 22:40:38 +0200

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by isa on 07.09.25.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
